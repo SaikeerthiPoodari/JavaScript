79e3522fa59d20c1f14446c116deaf85cd905df2
Saikeerthi <poodarisaikeerthi@gmail.com>
2025-09-22 11:02:00 +0530

    datatype

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var a=29;
-var a=18;  
-let b=31;
-const c=10;
-console.log(b);
-console.log(`${b} is the given number`)
-console.log(a);
+// var a=29;
+// var a=18;  
+// let b=31;
+// const c=10;
+// console.log(b);
+// console.log(`${b} is the given number`)
+// console.log(a);
+
+
+a=7897;
+console.log(typeof(a))
---
+++ datatype.js
@@ -0,0 +1,20 @@
+const { type } = require("express/lib/response");
+
+a=890;
+console.log(typeof a)
+username="keerthi";
+console.log(typeof username)
+c=true
+console.log(typeof c)
+data=null;
+console.log(typeof data)
+d=undefined;
+console.log(typeof d)
+
+//non primitive
+array= [71,"luck",true,null];
+console.log(typeof array)
+data = {
+    name:"dhee",age:"18",dept:"CSE"
+};
+console.log(typeof data)
